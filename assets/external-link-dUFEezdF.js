2fa4e7345186630d2130c5227b580e490b216d6c
rollwithdraw-web <rollwithdraw@yahoo.com>
2025-05-23 13:35:16 +0300

    Updates

---
+++ assets/external-link-dUFEezdF.js
@@ -0,0 +1,8 @@
+import{V as o}from"./CheckoutPage-DS0Cex5v.js";import"./index-C60RHSrd.js";import"./invoiceUtils-vytCLSo3.js";import"./gamepad-2-CNQfoblG.js";import"./shield-check-BBO7W4af.js";import"./file-text-DlHai6-P.js";const a=o`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M3.74 3.99a1 1 0 0 1 1-1H11a1 1 0 0 1 1 1v6.26a1 1 0 0 1-2 0V6.4l-6.3 6.3a1 1 0 0 1-1.4-1.42l6.29-6.3H4.74a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{a as externalLinkSvg};
